a0ed7286088256b2786fe88867f8da5f37756cc9
fabcoder547 <atharvjoshi8149@gmail.com>
2020-05-25 22:47:00 +0530

    Updates

---
+++ precache-manifest.77328daa8ca79ab8ed6df972d7c870b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99322c869f8927350121e8e029ee47be",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "ffb1d7836c05fbae47e5",
+    "url": "/TodoList/static/css/main.62fbae54.chunk.css"
+  },
+  {
+    "revision": "e1815c02c03c821c55d4",
+    "url": "/TodoList/static/js/2.3544ee40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.3544ee40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb1d7836c05fbae47e5",
+    "url": "/TodoList/static/js/main.c9fdec72.chunk.js"
+  },
+  {
+    "revision": "94fca36c9e77359bc4c0",
+    "url": "/TodoList/static/js/runtime-main.3e5670f1.js"
+  }
+]);
